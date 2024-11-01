4163623901a63e8d93f043fcf6a9e5f6d4b7baca
kamal <kamal@gmail.com>
2024-11-01 02:36:48 +0200

    2

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function positiveSum(arr) {
+    let list = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            list += arr[i]
+        }
+    }
+    return list
+}
+
+
+
+console.log(positiveSum([1, -4, 7, 12]));
