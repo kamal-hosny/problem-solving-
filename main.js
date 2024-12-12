fa87eba6f15eda8db2cfbc6be3283d40b3883447
kamal <kamal@gmail.com>
2024-12-12 15:46:22 +0200

    done 7

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-function countSheeps(sheep) {
-    let conter = 0
+function opposite(number) {
+    return -number
+  }
 
-    for (let i = 0; i < sheep.length; i++) {
-        if(sheep[i]){
-            conter ++
-        }
-        
-    }
-
-    return conter
-}
-
-console.log(countSheeps([true, true, true, false,
-    true, true, true, true,
-    true, false, true, false,
-    true, false, false, true,
-    true, true, true, true,
-    false, false, true, true]));
+  console.log(opposite(1));
