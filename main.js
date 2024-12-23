71e7ad49961efb4fc9a880184fd42b8d12f7d806
kamal <kamal@gmail.com>
2024-12-24 01:15:06 +0200

    13

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-function doubleChar(str) {
-  return [...str].map((x) => {
-    return x + x
-  }).join("")
+function basicOp(operation, value1, value2){
+  //Code
+  switch (operation) {
+    case "+" :
+      return value1 + value2
+      break;
+      case "-" :
+        return value1 - value2
+        break;
+      case "*" :
+        return value1 * value2
+        break;
+      case "/" :
+        return value1 / value2
+        break;
+    default:
+      break;
+  }
+  return clac ;
 }
-console.log(doubleChar("String"));
+
+console.log(basicOp('-', 4, 7));
