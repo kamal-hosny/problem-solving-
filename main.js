67e87ce2494455cd4fa77818fec4e6fe1134d24c
kamal <kamal@gmail.com>
2024-12-24 00:59:40 +0200

    done 12

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
-function countPositivesSumNegatives(input) {
-
-  if (!input || input.length === 0) {
-    return []
-  }
-
-  let positiveCount  = 0
-  let negativeSum  = 0
-  
-  for (let i = 0; i < input.length; i++) {
-    if (input[i] > 0) {
-      positiveCount++;
-    } else if (input[i] < 0) {
-      negativeSum += input[i];
-    }
-  }
-
-  return [positiveCount , negativeSum]
+function doubleChar(str) {
+  return [...str].map((x) => {
+    return x + x
+  }).join("")
 }
-
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+console.log(doubleChar("String"));
