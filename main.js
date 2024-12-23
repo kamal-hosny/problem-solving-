0549f9a8ce82008930dea87e169a6db4f1a61de2
kamal <kamal@gmail.com>
2024-12-23 23:04:52 +0200

    10 done

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function makeNegative(num) {
-    
-    return num > 0 ? -num : num
-    
-  }
-  console.log(makeNegative(1));
+function findNeedle(haystack) {
+  
+  return `found the needle at position ${haystack.indexOf("needle")}`
+}
+
+console.log(
+  findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
+);
