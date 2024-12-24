7b03e8a6cec1fc2f2151430f7096d8a651f8e891
kamal <kamal@gmail.com>
2024-12-24 16:50:39 +0200

    done 25

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-function monkeyCount(n) {
-   return Array.from({length: n}, (_, i) => i + 1)
-  }
+function greet(language) {
+  const languageDatabase = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso"
+  };
 
-  console.log(monkeyCount(4));
+  return languageDatabase[language] || languageDatabase["english"]
+
+}
+
+console.log(greet("english"
+));
