bbba7005051b63f0dc0315213217b0542496ed29
kamal <kamal@gmail.com>
2024-12-24 16:19:31 +0200

    done 23

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-
-function findAverage(array) {
-  if(!array || array == [] || array == null || array.length === 0) {
-    return 0
+const stringToNumber = function(str){
+  if(typeof str === "string"){
+    return Number(str)
   }
-  let sum = 0;
-  let count = array.length
-
-  for (let i = 0; i < array.length; i++) {
-     sum += array[i]
-    
-  }
-
-  let average = sum / count
-  return average
+  
 }
 
-console.log(findAverage([1,8,5,4]));
+console.log(stringToNumber("1234"));
