8b4125f1e9dd4a8ff06a9528621f5ca47410823e
kamal <kamal@gmail.com>
2024-12-24 18:46:17 +0200

    done 17

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 function switchItUp(number){
   
   switch (number) {
+    case 0:
+      return "Zero"
+      break;
     case 1:
       return "One"
+      break;
+    case 2:
+      return "Two"
       break;
     case 3:
       return "Three"
       break;
+    case 4:
+      return "Four"
+      break;
     case 5:
       return "Five"
       break;
-  
+    case 6:
+      return "Six"
+      break;
+    case 7:
+      return "Seven"
+      break;
+    case 8:
+      return "Eight"
+      break;
+    case 9:
+      return "Nine"
+      break;
+      default:
+        return "error"
   }
   }
 
-  console.log(switchItUp(1));
+  console.log(switchItUp(5));
