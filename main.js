cac5b60aa8b805f286cf73c4b06035e4de23d275
kamal <kamal@gmail.com>
2024-12-27 21:08:25 +0200

    done 42

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-function noBoringZeros(n) {
-  if (n === 0) return 0;
- let numArray =  Array.from(String(n), String)
+function getAverage(marks){
+  //TODO : calculate the downward rounded average of the marks array
 
-
- for (let i = 0; i < numArray.length + 1; i++) {
-  if(numArray[numArray.length - 1] == 0) {
-    numArray.pop()
+  if(marks.length === 0) {
+    return 0
   }
- }
- 
- return Number(numArray.join(""))
+
+  let Sum = marks.reduce((x , y) => {
+    return x + y
+  })
+  let Count = marks.length
+  let Average = Sum / Count  
+
+  return Math.floor(Average)
 
 }
 
-console.log(noBoringZeros(50));
+console.log(getAverage([1,5,87,45,8,8]));
