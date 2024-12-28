308f33f9eba4117bbefa0783ef72cb303f6aac90
kamal <kamal@gmail.com>
2024-12-28 04:02:33 +0200

    done 52

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-function removeChar(str){
-  
-  let result = []
- 
-  for (let i = 1; i < str.length -1; i++) {
-    result.push(str[i])
+function squareSum(numbers){
+if( numbers.length < 1){
+  return 0
+}
+
+let result = 0
+let squr1 = []
+  for (let y = 0; y < numbers.length; y++) {
+    squr1.push(numbers[y] ** 2)
     
   }
+  for (let i = 0; i < squr1.length; i++) {
+    result += squr1[i]
+  }
+return result
 
-  return result.join("")
 
- }; 
+// return numbers.map((x) => {
+//   return x ** 2
+// }).reduce((x, y) => {
+// return x + y
+// }, 0)
 
- console.log(removeChar("eloquent"));
+}
+
+console.log(squareSum([1, 2]));
