e603e57d20294c37f2ea9617b94ac1b7843795fe
kamal <kamal@gmail.com>
2024-12-29 03:07:02 +0200

    done 59

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-function simpleMultiplication(number) {
-
-  if (number % 2 === 0) {
-    return number * 8
-  } {
-    return number * 9
-  }
-
+function makeUpperCase(str) {
+  return str.toUpperCase()
 }
-console.log(simpleMultiplication(3));
+
+console.log(makeUpperCase("hello"));
