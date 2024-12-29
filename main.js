3339842d797e90d0af7592b23932fbe954ed3845
kamal <kamal@gmail.com>
2024-12-29 03:13:39 +0200

    done 60

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function makeUpperCase(str) {
-  return str.toUpperCase()
+function arrayPlusArray(arr1, arr2) {
+  
+  let arr = []
+
+  
+
+  return arr.concat(arr1).concat(arr2).reduce((x, y) => {
+    return x + y 
+  })
 }
 
-console.log(makeUpperCase("hello"));
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
