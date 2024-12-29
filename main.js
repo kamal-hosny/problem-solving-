29a68ddf72c858c802cab15dab8b2a71acca4d98
kamal <kamal@gmail.com>
2024-12-29 02:02:42 +0200

    done 56

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-function maps(x){
+function fakeBin(x){
 
-  return x.map((y) => {
-    return y * 2
-  })
+  let busket = []
+
+  for (let i = 0; i < x.length; i++) {
+    if(x[i] >= 5){
+      busket.push("1")
+    }else{
+      busket.push("0")
+    }
+  }
+
+  return busket.join("")
+
 }
 
-console.log((maps([1, 2, 3])));
+console.log(fakeBin('45385593107843568'));
