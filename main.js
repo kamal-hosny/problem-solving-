5e616c7aad4e7abd94035a9aca8498f1a8b4d9ca
kamal <kamal@gmail.com>
2024-12-31 16:09:04 +0200

    done 72

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 function betterThanAverage(classPoints, yourPoints) {
-
   let sum = classPoints.reduce((x, y) => {
     return x + y
   })
   let count = classPoints.length;
-
   let average = sum / count
-
   // Your code here
   if(yourPoints < average) {
     return false
   }else {
     return true
   } 
-
 }
 
 console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50));
