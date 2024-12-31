1d84bb788e5bf261f8987bd15c0b949b576c8adb
kamal <kamal@gmail.com>
2024-12-31 16:18:56 +0200

    done 72

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-function betterThanAverage(classPoints, yourPoints) {
-  let sum = classPoints.reduce((x, y) => {
-    return x + y
-  })
-  let count = classPoints.length;
-  let average = sum / count
-  // Your code here
-  if(yourPoints < average) {
-    return false
-  }else {
-    return true
-  } 
+function reverseWords(str){
+  
+  return str.trim().split(" ").reverse().join(" ")
 }
 
-console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50));
+console.log(reverseWords("hello world!"));
