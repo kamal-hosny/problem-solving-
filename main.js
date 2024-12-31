d9269f69dda485fd063b9030198c07b27bf3832e
kamal <kamal@gmail.com>
2024-12-31 16:07:29 +0200

    done 71

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-function correct(string){
-	
-  return [...string].map((x) => {
-    switch (x) {
-      case "5":
-        return "S"
-      case "0":
-        return "O"
-      case "1":
-        return "I"
-    
-      default :
-        return x 
-    }
-  }).join("")
+function betterThanAverage(classPoints, yourPoints) {
+
+  let sum = classPoints.reduce((x, y) => {
+    return x + y
+  })
+  let count = classPoints.length;
+
+  let average = sum / count
+
+  // Your code here
+  if(yourPoints < average) {
+    return false
+  }else {
+    return true
+  } 
+
 }
 
-console.log(correct("L0ND0N"));
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50));
