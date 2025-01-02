e814973f8727f8461e7ddc3127684935ae761383
kamal <kamal@gmail.com>
2025-01-02 21:08:12 +0200

    done 80

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function evenOrOdd(number) {
-  if(number % 2 == 0){
-    return 'Even'
-  }else{
-    return 'Odd'
-  }
-}
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let basket = []
 
-console.log(evenOrOdd(2));
+  for (let i = 0; i < birds.length; i++) {
+    if(!geese.includes(birds[i])){
+      basket.push(birds[i])
+    }
+    
+  }
+return basket
+  // return an array containing all of the strings in the input array except those that match strings in geese
+};
+
+console.log(gooseFilter( ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]));
