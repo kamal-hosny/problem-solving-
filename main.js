b5cf231f6dcb60fad06bd106cafbeab1b45a729c
kamal <kamal@gmail.com>
2025-01-03 20:31:22 +0200

    done 99

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-const areaOrPerimeter = function(l , w) {
-    // Return your answer
+// For every good kata idea there seem to be quite a few bad ones!
 
-    if(l == w) {
-        return l * w
-    } else {
-        return (l + w )  * 2 
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+function well(x){
+
+    let busketBad = []
+    let busketGood = []
+
+    for (let i = 0; i < x.length; i++) {
+        if(x[i] === "good"){
+            busketGood.push(x[i])
+        }else {
+            busketBad.push(x[i])
+        }
     }
 
-  };
+    switch (true) {
+        case busketGood.length === 0:
+            return 'Fail!'
 
-  console.log(areaOrPerimeter(3, 3));
+        case busketGood.length <= 2:
+            return 'Publish!'
+
+        case busketGood.length > 2:
+            return 'I smell a series!'
+    
+        default:
+            break;
+    }
+
+
+}
+
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad']));
