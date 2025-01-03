74646aa00a86ed2c45d6e8d0a098ac52e48ba4b6
kamal <kamal@gmail.com>
2025-01-03 02:15:37 +0200

    done 85

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-function modifiedSum(a, n) {
-    
-    let squared = 0
-    let total = a.reduce((x, y) =>{
-        return x + y
-    })
-    for (let i = 0; i < a.length; i++) {
-
-        squared += a[i] ** n
-        
-    }
-
-    let finel = squared - total
-
-    return finel
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
   }
 
-  console.log(modifiedSum([1, 2, 3], 3));
+  console.log(goals(5, 10, 2 ));
