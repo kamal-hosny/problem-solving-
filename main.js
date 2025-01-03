467fcdf9876b41cda28c45e0b227e977f0e4f04b
kamal <kamal@gmail.com>
2025-01-03 19:19:37 +0200

    done 95

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+// This function should test if the factor is a factor of base.
 
-// Write a program that returns the girl's age (0-9) as an integer.
+// Return true if it is a factor or false if it is not.
 
-// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+// About factors
+// Factors are numbers you can multiply together to get another number.
 
-function getAge(inputString){
-return Number(inputString.split(" ")[0])
-}
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
 
-console.log(getAge("4 years old"));
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+function checkForFactor (base, factor) {
+
+    if(base % factor == 0) {
+        return true
+    }else {
+        return false
+    }
+
+    // code here
+  }
+
+  console.log(checkForFactor(9,2));
