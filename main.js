ff355ab056e362692450eb7d40cb75668cdf7cce
kamal <kamal@gmail.com>
2025-01-03 18:56:36 +0200

    93

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
-// This is a spin off of my first kata.
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
 
-// You are given a string containing a sequence of character sequences separated by commas.
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
 
-// Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
 
-// If the input string is empty or the removal of the first and last items would cause the resulting string to be empty, return an empty value (represented as a generic value NULL in the examples below).
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
 
-function array(string) {
-    // TODO
-    
-    let text = string.split(",")
+function pipeFix(numbers){
+    let start = numbers[0]
+    let last = numbers[numbers.length - 1]
 
-    if(text.length < 3) {
-        return null
-    }
+    let basket = [];
 
-    let basket = []
-
-    for (let i = 1; i < text.length -1; i++) {
-        
-        basket.push(text[i])
+    for (let i = start; i <= last; i++) {
+        basket.push(i)
         
     }
-    return basket.join(" ")
-  }
 
-  console.log(array('1,2,3,4'));
+return basket
+}
+
+console.log(pipeFix([1,3,5,6,7,8]));
