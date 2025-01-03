c08c3b564413962c83ada21a6c0ad25b47747256
kamal <kamal@gmail.com>
2025-01-03 16:27:09 +0200

    done 88

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function findMultiples(integer, limit) {
-   
-    let basket = [];
+function distinct(a) {
+    
+    let basket = []
 
-    for (let i = integer; i <= limit; i+= integer) {
-        
-        basket.push(i)
-        
+    for (let i = 0; i < a.length; i++) {
+        if(!basket.includes(a[i])){
+            basket.push(a[i])
+        }
     }
 
     return basket
+    
   }
-  
-  console.log(findMultiples(5, 7));
+
+  console.log(distinct([1, 2, 1, 1, 3, 2]));
